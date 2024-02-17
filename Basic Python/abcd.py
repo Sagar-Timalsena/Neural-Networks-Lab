cb21ae1163d6479847e6fa6ec069c1f31543e954
SAAGAR <38255285+Sagar-Timalsena@users.noreply.github.com>
2024-02-17 10:42:20 +0545

    small changes

---
+++ Basic Python/abcd.py
@@ -0,0 +1,30 @@
+# Variable:
+
+# A variable is a name given to a memory location in a program 
+
+# name = "Saagar"
+# age= 25
+# price= 100
+
+# print(name,age,price)
+
+# print("my name is : ", name )
+# print(type(name))
+
+
+# Data Types:
+# Integer +ve, -ve , 0
+# String  --> "" / ' ' /''' '''
+# float 3.1, 9.0
+# boolean -> True and false / T F
+# None a= none 
+
+# age= 23
+# old =False
+# a = None
+# print(type(old))
+# print(type(a))
+
+#keywords
+# Keywords are reserved words in python 
+# and as assert break class continue def del elif else except finally false for from global if import in is lambda nonlocal none not or pass raise return true try with while yield 
