0a93a65d323073579f81d37dcd499044358bb712
SAAGAR <38255285+Sagar-Timalsena@users.noreply.github.com>
2024-02-17 16:49:56 +0545

    added some code

--- Basic Python/abcd.py
+++ Basic Python/abcd.py
@@ -28,3 +28,78 @@
 #keywords
 # Keywords are reserved words in python 
 # and as assert break class continue def del elif else except finally false for from global if import in is lambda nonlocal none not or pass raise return true try with while yield 
+
+# print sum
+"""
+a = 10
+b =200
+sum= a + b
+difference = a-b
+multiply = a * b
+division = b/a
+
+print(sum,difference,multiply,division)
+"""
+
+
+# Typed Language
+# implicit and explicit 
+
+""" Types of Tokens
+Punctuators
+Pubctuators are symbols to organize sentence structure in programming
+ (),{},@, [], # """
+
+# comment => # 
+"""
+          commment part
+"""
+
+"""
+          Input in Python 
+"""
+# name = input("name: ")
+# print("Hello " + name)
+# age = int(input("age: "))
+# print ("Price of Suger can is ",  age)
+
+"""
+          if / elif program
+
+age = int(input("age : "))
+if(age <=1 ):
+          print("ChildA")
+
+elif(age >=1 and age < 10): 
+          print("primary School Student")
+
+elif(age >= 10 and age < 20): 
+          print("High School Student")
+else:
+          print("Adult")
+
+"""
+
+
+"""
+Conditional Statement
+
+Single Line If/ Ternary Operator
+
+
+food = input("food: ")
+eat = "Yes" if food == "cake " else "no"
+
+print(eat)
+
+
+food = input("food: ")
+print("Sweet") if food == "cake" or food == "popcon" else print("not eat")
+
+cleaver if/ Ternary Operator 
+
+"""
+
+
+
+
